56701d171a0d1fd85f1a02bf4a4b822ca3c7df41
Aurorah Harmony <aurorahHarmony@gmail.com>
2024-06-22 21:26:26 +1200

    init home page

--- _config.ts
+++ _config.ts
@@ -1,16 +1,29 @@
 import lume from "lume/mod.ts";
 import favicon from "lume/plugins/favicon.ts";
-import jsx_preact from "lume/plugins/jsx_preact.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 
-const site = lume();
+import tailwindOptions from "./tailwind.config.js";
+
+const site = lume({
+  location: new URL("https://scriptronix.net"),
+  src: "./src",
+  server: {
+    page404: "/404/",
+  },
+});
+
+site.copy("static");
+site.copy("static/img");
+site.copy("src/static", "static");
 
 site.use(favicon());
-site.use(jsx_preact());
-site.use(tailwindcss());
-site.use(tailwindcss());
+site.use(
+  tailwindcss({
+    extensions: [".vto"],
+    options: tailwindOptions,
+  })
+);
 site.use(postcss());
 
 export default site;
